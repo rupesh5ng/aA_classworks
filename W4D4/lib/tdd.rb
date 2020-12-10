63ca8170ead4e0caf8d0cb0350208771919c23e7
Chubbibanana <dongsoo.tchah@gmail.com>
2020-12-10 13:59:59 -0800

    Wrote spec for #two_sum

---
+++ W4D4/lib/tdd.rb
@@ -0,0 +1,4 @@
+def my_uniq(arr)
+    return arr if arr.length <= 1
+    arr.uniq
+end
--- W4D4/spec/tdd_rspec.rb
+++ W4D4/spec/tdd_rspec.rb
@@ -7,7 +7,7 @@ describe "my_uniq" do
     it " return an array with uniq element" do
         arr = [1,2,1,3,3]
         result = arr.uniq
-        expect{ arr.my_uniq}.to eq(result)
+        expect(my_uniq(arr)).to eq(result)
 
     end
 
@@ -15,8 +15,31 @@ describe "my_uniq" do
         it " should return same array" do 
             arr =[]
 
-            expect{ arr.my_uniq}.to be(arr)
+            expect(my_uniq(arr)).to be(arr)
         end
 
     end 
+end
+
+describe "#two_sum" do
+    subject(:array) {%w(-1 0 2 -2 1)}
+    # context "When array is empty or array length is less than 2" do
+    #     arr2 = []
+    #     it "returns nil if array is empty" do
+    #         expect(arr2.empty?).to raise_error("Array doesn't have elements")
+    #     end
+    #     it "returns "
+    # end
+
+    context "When there are matching pairs" do
+        it "returns index pairs that add to 0" do
+            expect(array.two_sum).to eq([[0, 4], [2, 3]])
+        end
+    end
+
+    context "When there are no matching pairs" do
+        it "returns nil if no match is found" do
+            expect([].two_sum).to eq(nil)
+        end 
+    end
 end
