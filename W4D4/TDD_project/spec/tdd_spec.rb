5dc56482d9fd26a1d04bdb0fa505ff04cfa8da61
your name here <rupesh_singh87@yahoo.com>
2020-12-10 14:35:01 -0600

    worked on my_uniq problem

---
+++ W4D4/TDD_project/spec/tdd_spec.rb
@@ -0,0 +1,21 @@
+require "rspec"
+require "tdd"
+
+
+describe "my_uniq"
+
+    it " return an array with uniq element" do
+        arr = [1,2,1,3,3]
+        result = arr.uniq
+        expect{ arr.my_uniq}.to eq(result)
+
+    end
+    context "when given array is empty or length == 1" do
+        it " should return same array" do 
+            arr =[]
+
+            expect{ arr.my_uniq}.to be(arr)
+        end
+
+    end 
+end
