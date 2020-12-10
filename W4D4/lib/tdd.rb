a0eb98e378f203232d532cd520748f03f026a2f9
your name here <rupesh_singh87@yahoo.com>
2020-12-10 16:23:55 -0600

    finised matrix_sum rspec

--- W4D4/lib/tdd.rb
+++ W4D4/lib/tdd.rb
@@ -2,3 +2,25 @@ def my_uniq(arr)
     return arr if arr.length <= 1
     arr.uniq
 end
+
+class Array
+    def two_sum
+        pairs = []
+        (0...self.length - 1).each do |i|
+            (i+1...self.length).each do |j|
+                pairs << [i,j] if (self[i] + self[j]) == 0
+            end
+        end
+
+        return nil if pairs.empty?
+        pairs
+    end
+
+end
+# arr = %w(-1 0 2 -2 1)
+# p arr
+# p arr.two_sum
+
+def my_transpose(matrix)
+    
+end
