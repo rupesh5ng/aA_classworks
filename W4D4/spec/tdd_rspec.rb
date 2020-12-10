b80fbb654591a3923bd42ee5c6d7f81957754167
Chubbibanana <dongsoo.tchah@gmail.com>
2020-12-10 13:38:44 -0800

    Finished setup for rspec

--- W4D4/spec/tdd_rspec.rb
+++ W4D4/spec/tdd_rspec.rb
@@ -1,9 +1,8 @@
-require "spec"
-
 require "tdd"
+require "rspec"
 
 
-describe "my_uniq"
+describe "my_uniq" do
 
     it " return an array with uniq element" do
         arr = [1,2,1,3,3]
@@ -11,6 +10,7 @@ describe "my_uniq"
         expect{ arr.my_uniq}.to eq(result)
 
     end
+
     context "when given array is empty or length == 1" do
         it " should return same array" do 
             arr =[]
