7be8f03ce7789ee985f71705c15a7c35ed82a019
your name here <rupesh_singh87@yahoo.com>
2020-12-11 18:52:40 -0600

    finished phase II

--- W4D5/windowed_max_range.rb
+++ W4D5/windowed_max_range.rb
@@ -34,10 +34,10 @@ def windowed_max_range(arr, window_size)
 end
 
 
-p windowed_max_range([1, 0, 2, 5, 4, 8], 2) #== 4 # 4, 8
-p windowed_max_range([1, 0, 2, 5, 4, 8], 3) #== 5 # 0, 2, 5
-p windowed_max_range([1, 0, 2, 5, 4, 8], 4) #== 6 # 2, 5, 4, 8
-p windowed_max_range([1, 3, 2, 5, 4, 8], 5) #== 6 # 3, 2, 5, 4, 8
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 2) #== 4 # 4, 8
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 3) #== 5 # 0, 2, 5
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 4) #== 6 # 2, 5, 4, 8
+# p windowed_max_range([1, 3, 2, 5, 4, 8], 5) #== 6 # 3, 2, 5, 4, 8
 
 
 # #Phase 2: Queue
@@ -48,23 +48,57 @@ class MyQueue
     end
 
     def peek #returns the "top" or "next" item without actually removing it
-    
+        @store.first
     end
 
     def size
-
+        @store.length
     end
 
     def empty?
-
+        @store.empty?
     end
 
-    def enqueue #adds an element to the back of the queue
-
+    def enqueue(ele) #adds an element to the back of the queue
+        @store.push(ele)
     end
 
     def dequeue #removes an element from the front of the queue and returns it
-
+        @store.shift
     end
   end
 
+#   q = MyQueue.new 
+#   p q.size
+#   p q.enqueue("ele")
+class MyStack
+    def initialize
+        @store =[]
+    end
+
+    def peek
+        @store.last
+    end
+
+    def size
+        @store.length
+    end
+
+    def empty?
+        @store.empty?
+    end
+
+    def pop
+        @store.pop
+    end
+
+    def push(ele)
+        @store.push(ele)
+    end
+
+end
+
+class StackQueue < MyStack
+    super
+    
+end
