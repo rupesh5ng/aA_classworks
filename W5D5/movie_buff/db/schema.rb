b9677bfe3df1c58c7f9d9ded4b64f74c75719029
your name here <rupesh_singh87@yahoo.com>
2020-12-18 14:23:20 -0600

    finished first 3 problem

--- W5D5/movie_buff/db/schema.rb
+++ W5D5/movie_buff/db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20170711160807) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "plpgsql" 
 
   create_table "actors", force: :cascade do |t|
     t.string "name", null: false
--- W5D5/movie_buff/movie_buff/01_queries.rb
+++ W5D5/movie_buff/movie_buff/01_queries.rb
@@ -6,6 +6,9 @@ def it_was_ok
   # We can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
+  Movie
+    .select( :id, :title , :score)
+    .where(score: 2..3)
 
 end
 
@@ -20,6 +23,13 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
+  Movie
+    .select(:id,:title)
+    .joins(:actors)
+    .where(actors: { name: 'Harrison Ford' })
+    .where.not(castings: {ord: 1})
+
+
 
 end
 
@@ -37,6 +47,12 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
+  Movie
+  .select(:id , :title)
+  .join(:actors)
+  .group("movies.id")
+  .order("COUNT(actors.id) desc")
+  .limit(3)
 
 end
 
