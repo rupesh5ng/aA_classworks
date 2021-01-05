9af747d11075a172d61cb814799e3ac543f61579
rupesh5ng <rupesh_singh87@yahoo.com>
2021-01-05 17:26:16 -0600

    finished with user controller/erb portion

--- W7D2/MusicApp/app/controllers/users_controller.rb
+++ W7D2/MusicApp/app/controllers/users_controller.rb
@@ -10,12 +10,24 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.create(user_params)
+        if @user.save
+            login(@user)
+            redirect_to user_url(@user)
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
     end
 
     def show
+        @user = User.find_by(id: params[:id])
+        render :show
     end
 
     def edit 
+        @user = User.find_by(id: params[:id])
+        render :edit
     end
     def update 
     end
